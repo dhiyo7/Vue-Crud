3d1b10fb9dff8440cf3e9c3ccf66fe4118266e62
dhiyo7 <dhiyoreksa12@gmail.com>
2022-07-19 22:25:10 +0700

    [DHIYO][ADD] - First Commit

---
+++ src/components/Test.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="container px-5 py-24 mx-auto">
+    <div class="flex flex-col text-center w-full mb-20">
+      <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Diari Jajan Februari</h1>
+      <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Pengeluaran Bulan Ini Rp. 5000.000</p>
+      <button
+          class="flex mx-auto mt-6 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+        Tambah Item
+      </button>
+      <div class="flex flex-wrap -m-4 mt-10 ">
+        <div class="xl:w-1/4 md:w-1/2 p-4">
+          <div class="bg-gray-100 p-6 rounded-lg">
+            <div class="flex border-t border-gray-200 py-2">
+              <span class="text-gray-500">Color</span>
+              <span class="ml-auto text-gray-900">Blue</span>
+            </div>
+          </div>
+        </div>
+        <div class="xl:w-1/4 md:w-1/2 p-4">
+          <div class="bg-gray-100 p-6 rounded-lg">
+            <div class="flex border-t border-gray-200 py-2">
+              <span class="text-gray-500">Color</span>
+              <span class="ml-auto text-gray-900">Blue</span>
+            </div>
+          </div>
+        </div>
+        <div class="xl:w-1/4 md:w-1/2 p-4">
+          <div class="bg-gray-100 p-6 rounded-lg">
+            <div class="flex border-t border-gray-200 py-2">
+              <span class="text-gray-500">Color</span>
+              <span class="ml-auto text-gray-900">Blue</span>
+            </div>
+          </div>
+        </div>
+        <div class="xl:w-1/4 md:w-1/2 p-4">
+          <div class="bg-gray-100 p-6 rounded-lg">
+            <div class="flex border-t border-gray-200 py-2">
+              <span class="text-gray-500">Color</span>
+              <span class="ml-auto text-gray-900">Blue</span>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Test"
+}
+</script>
+
+<style scoped>
+
+</style>
